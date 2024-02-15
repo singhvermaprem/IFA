a05b985eb0e03a5e342faeb318d0b8a34e4225b0
singvermaprem <pradhumansinghverma@gmail.com>
2024-02-16 04:40:42 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+const app = new express();
+const port = 3000;
+
+
+
+app.listen(port, ()=>{
+    console.log(`server has been started on port ${port}`)
+});
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) =>
+{
+    res.render("index.ejs");
+});
+
+app.get("/shop.ejs", (req, res) =>
+{
+    res.render("shop.ejs");
+});
+
+app.get("/productDetails.ejs", (req, res) =>
+{
+    res.render("productDetails.ejs");
+});
+
+
+
+
+
---
+++ public/shoppageScript.js
@@ -0,0 +1,2 @@
+document.querySelector("#navbar > li:nth-child(1) > a").classList.remove('active')
+document.querySelector("#navbar > li:nth-child(2) > a").classList.add('active')
