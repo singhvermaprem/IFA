f435785afaaf6de01ba575eddbdb0e786761da77
singvermaprem <pradhumansinghverma@gmail.com>
2024-02-16 06:21:20 +0530

    test

--- index.js
+++ index.js
@@ -14,20 +14,6 @@ const PORT = process.env.PORT || 3000;
 
 server.listen(PORT , () => console.log("server is running on port 3000."))
 
-app.use(express.static('public'));
-
-
-app.get("/shop.ejs", (req, res) =>
-{
-    res.render("shop.ejs");
-});
-
-app.get("/productDetails.ejs", (req, res) =>
-{
-    res.render("productDetails.ejs");
-});
-
-
 
 
 
