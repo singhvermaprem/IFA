8bb57584615422c7a6bb45b3e8df7597e375e13e
singvermaprem <pradhumansinghverma@gmail.com>
2024-02-16 06:23:32 +0530

    test 2

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const app = new express();
 
 const server = http.createServer((req, res) =>
 {
-    res.render("index.ejs");
+    res.render("C:\Web Development Project\IFA Ecommerce\views\index.ejs");
 });
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT , () => console.log("server is running on port 3000."))
+server.listen(PORT , () => console.log("prem server is running on port 3000."))
 
 
 
