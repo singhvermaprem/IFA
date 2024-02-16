8c50767313c2fb40129d21223b29f1f8ff628323
singvermaprem <pradhumansinghverma@gmail.com>
2024-02-16 11:55:56 +0530

    Final Project files.

--- public/pdetails.js
+++ public/pdetails.js
@@ -20,3 +20,36 @@ smallImage[3].onclick = function()
 {
     mainImage.src= smallImage[3].src
 }
+
+
+document.querySelector("#navbar > li:nth-child(1) > a").classList.remove('active')
+document.querySelector("#navbar > li:nth-child(2) > a").classList.add('active')
+
+
+
+const close = document.getElementById('close');
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const shop = document.querySelector("#navbar > li:nth-child(2) > a");
+const home = document.querySelector("#navbar > li:nth-child(1) > a")
+if(bar)
+{
+    bar.addEventListener("click", () => {
+        nav.classList.add('active')
+    });
+}
+
+if(close)
+{
+    close.addEventListener("click", () => {
+        nav.classList.remove('active')
+    });
+}
+
+if(shop)
+{
+    shop.addEventListener("click", () => {
+        home.classList.remove('active')
+        shop.classList.add('active')
+    });
+}
--- public/shoppageScript.js
+++ public/shoppageScript.js
@@ -1,2 +1,31 @@
 document.querySelector("#navbar > li:nth-child(1) > a").classList.remove('active')
 document.querySelector("#navbar > li:nth-child(2) > a").classList.add('active')
+
+
+const close = document.getElementById('close');
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const shop = document.querySelector("#navbar > li:nth-child(2) > a");
+const home = document.querySelector("#navbar > li:nth-child(1) > a")
+if(bar)
+{
+    bar.addEventListener("click", () => {
+        nav.classList.add('active')
+    });
+}
+
+if(close)
+{
+    close.addEventListener("click", () => {
+        nav.classList.remove('active')
+    });
+}
+
+if(shop)
+{
+    shop.addEventListener("click", () => {
+        home.classList.remove('active')
+        shop.classList.add('active')
+    });
+}
+
